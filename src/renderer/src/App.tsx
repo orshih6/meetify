211886a661eff441cfe4bd85d52fa98d4ec118cb
orshih6 <orshihorshhoo@gmail.com>
2026-06-20 19:13:32 +0800

    initial created

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,33 @@
+import Versions from './components/Versions'
+
+function App(): React.JSX.Element {
+  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="text-2xl font-bold">Meetify</div>
+      <div className="text">
+        Build an Electron app with <span className="react">React</span>
+        &nbsp;and <span className="ts">TypeScript</span>
+      </div>
+      <p className="tip">
+        Please try pressing <code>F12</code> to open the devTool
+      </p>
+      <div className="actions">
+        <div className="action">
+          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
+            Documentation
+          </a>
+        </div>
+        <div className="action">
+          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
+            Send IPC
+          </a>
+        </div>
+      </div>
+      <Versions></Versions>
+    </div>
+  )
+}
+
+export default App
